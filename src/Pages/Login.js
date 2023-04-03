89499bf31e7d3f0fbc922a9e56b1ace28ff8efc2
devpedroval <prmval@gmail.com>
2023-04-03 17:06:45 -0300

    realizando pagina de login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    buttonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+    if (this.validation()) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  };
+
+  validation = () => {
+    const { email, password } = this.state;
+    const magicNumber = 5;
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const returnRegex = regex.test(email);
+    const returnPass = password.length > magicNumber;
+    return !!(returnPass && returnRegex);
+  };
+
   render() {
+    const { email, password, buttonDisabled } = this.state;
     return (
-      <div>Login</div>
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="login-submit-btn"
+          disabled={ buttonDisabled }
+        >
+          Entrar
+
+        </button>
+      </form>
     );
   }
 }
