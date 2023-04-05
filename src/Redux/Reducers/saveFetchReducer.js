95d900356dd193d9b4396b2e4d50c5e3765b57e2
devpedroval <prmval@gmail.com>
2023-04-05 13:43:58 -0300

    iniciando requisito 14

--- src/Redux/Reducers/saveFetchReducer.js
+++ src/Redux/Reducers/saveFetchReducer.js
@@ -1,15 +1,19 @@
 import { SAVE_FETCH_ACTION } from '../Actions';
 
 const INITIAL_STATE = {
-  result: {},
+  result: [],
 };
+const maxItensForExibition = 12;
 
 export const saveFetchReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case SAVE_FETCH_ACTION:
     return {
       ...state,
-      result: action.payload,
+      result: (action.payload.drinks && action.payload.drinks
+        .slice(0, maxItensForExibition))
+      || (action.payload.meals && action.payload.meals.slice(0, maxItensForExibition))
+      || [],
     };
   default:
     return state;
