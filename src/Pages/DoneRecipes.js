18a756bde2667fa1744afe413ffc8066ccbcb5a8
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-04-05 17:43:59 -0300

    Requisito 44 - correção

--- src/Pages/DoneRecipes.js
+++ src/Pages/DoneRecipes.js
@@ -11,31 +11,32 @@ export default class DoneRecipes extends Component {
         <button data-testid="filter-by-all-btn">All</button>
         <button data-testid="filter-by-meal-btn">Meals</button>
         <button data-testid="filter-by-drink-btn">Drinks</button>
-        {doneRecipes.map(({ category, name, image, doneDate, tags }, index) => (
-          <div key={ index }>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              src={ image }
-              alt={ name }
-            />
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              {category}
-            </p>
+        {doneRecipes && doneRecipes
+          .map(({ category, name, image, doneDate, tags }, index) => (
+            <div key={ index }>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ image }
+                alt={ name }
+              />
+              <p data-testid={ `${index}-horizontal-top-text` }>
+                {category}
+              </p>
 
-            <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+              <p data-testid={ `${index}-horizontal-name` }>{name}</p>
 
-            <p data-testid={ `${index}-horizontal-done-date` }>
-              {doneDate}
-            </p>
-            <button data-testid={ `${index}-horizontal-share-btn` }>Share</button>
-            {tags
+              <p data-testid={ `${index}-horizontal-done-date` }>
+                {doneDate}
+              </p>
+              <button data-testid={ `${index}-horizontal-share-btn` }>Share</button>
+              {tags
               && tags.map((elem, indexTag) => (
                 <p key={ indexTag } data-testid={ `${index}-${elem}-horizontal-tag` }>
                   {elem}
                 </p>
               ))}
-          </div>
-        ))}
+            </div>
+          ))}
       </div>
     );
   }
