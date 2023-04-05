fb0e8cabd8c435015b4c9819ab4256a19d32dae5
devpedroval <prmval@gmail.com>
2023-04-05 19:07:28 -0300

    iniciando carrossel

--- src/Components/Meal.js
+++ src/Components/Meal.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+import '../App.css';
 
 export default class Meal extends Component {
   state = {
@@ -53,49 +55,75 @@ export default class Meal extends Component {
     return (
       loading ? null : (
         meal.map((iten) => (
-          <div key={ iten.strMeal }>
-            <h1
-              data-testid="recipe-title"
-            >
-              {iten.strMeal}
-            </h1>
-            <h3
-              data-testid="recipe-category"
-            >
-              {iten.strCategory}
-
-            </h3>
-            <img
-              data-testid="recipe-photo"
-              src={ iten.strMealThumb }
-              alt={ iten.strMeal }
-            />
-            {ingredients.map((ingredient, index) => (
-              <p
-                data-testid={ `${index}-ingredient-name-and-measure` }
-                key={ ingredient }
+          <>
+            <div key={ iten.strMeal }>
+              <h1
+                data-testid="recipe-title"
               >
-                {ingredient}
+                {iten.strMeal}
+              </h1>
+              <h3
+                data-testid="recipe-category"
+              >
+                {iten.strCategory}
 
+              </h3>
+              <img
+                data-testid="recipe-photo"
+                src={ iten.strMealThumb }
+                alt={ iten.strMeal }
+              />
+              {ingredients.map((ingredient, index) => (
+                <p
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                  key={ ingredient }
+                >
+                  {ingredient}
+
+                </p>
+              ))}
+              <p
+                data-testid="instructions"
+              >
+                {iten.strInstructions}
               </p>
-            ))}
-            <p
-              data-testid="instructions"
-            >
-              {iten.strInstructions}
-            </p>
-            <iframe
-              data-testid="video"
-              width="560"
-              height="315"
-              src={ iten.strYoutube }
-              title="YouTube video player"
-              allowFullScreen
-            />
-            {loading ? null : carrousel.length }
-            {/* Código acima onde deverá entrar o carrosel,
-             deixei dessa maneira apenas para passar sem erro de lint */}
-          </div>
+              <iframe
+                data-testid="video"
+                width="560"
+                height="315"
+                src={ iten.strYoutube }
+                title="YouTube video player"
+                allowFullScreen
+              />
+            </div>
+            <div className="carDiv">
+              <motion.div className="carrousel" whileDrag={ { cursor: 'grabbing' } }>
+                <motion.div
+                  className="inner"
+                  drag="x"
+                >
+                  {carrousel.map((drink, index) => (
+                    <motion.div
+                      className="iten"
+                      data-testid={ `${index}-recommendation-card` }
+                      key={ drink.idDrink }
+                    >
+                      <img
+                        // data-testid={ `${index}-recommendation-title` }
+                        src={ drink.strDrinkThumb }
+                        alt={ drink.idDrink }
+                      />
+                      <p data-testid={ `${index}-recommendation-title` }>
+                        {iten.strDrink}
+                      </p>
+
+                    </motion.div>
+                  ))}
+                </motion.div>
+
+              </motion.div>
+            </div>
+          </>
         ))
       )
     );
