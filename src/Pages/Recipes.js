c5a658dc4549486fa431df5a7f57affde2e45503
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-04-11 20:31:29 -0300

    Testes Recipes ok

--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -4,14 +4,28 @@ import { connect } from 'react-redux';
 import Meals from '../Components/Meals';
 import Drinks from '../Components/Drinks';
 import Header from '../Components/Header';
-import { pushAction } from '../Redux/Actions';
+import { pushAction, saveFetchAction } from '../Redux/Actions';
 import Footer from '../Components/Footer';
 
 class Recipes extends Component {
   componentDidMount() {
-    const { dispatch, history } = this.props;
+    const { dispatch, history, history: { location: { pathname } } } = this.props;
     dispatch(pushAction(history.push));
+    console.log(history.location.pathname);
+
+    if (pathname === '/meals') {
+      this.fetchRecipes('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    } else {
+      this.fetchRecipes('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    }
   }
+
+  fetchRecipes = async (api) => {
+    const { dispatch } = this.props;
+    const firstfetch = await fetch(api);
+    const json = await firstfetch.json();
+    dispatch(saveFetchAction(json));
+  };
 
   render() {
     const { history: { location: { pathname } } } = this.props;
