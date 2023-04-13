2beba69a9d05cfc5c31ed1e163f970ef5416b546
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-04-13 19:28:28 -0300

    Tela profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
+import iconProfile from '../images/Perfil.png';
+import iconLogout from '../images/logoutIcon.png';
+import iconFav from '../images/favIcon.png';
+import iconDone from '../images/doneIcon.png';
+import '../Styles/Profile.css';
 
 export default class Profile extends Component {
   handleClick = (rota) => {
@@ -18,35 +23,70 @@ export default class Profile extends Component {
   render() {
     const emailUser = JSON.parse(localStorage.getItem('user'));
     return (
-      <div>
+      <div className="tela">
         <Header name="Profile" />
-        Profile
-        <h3 data-testid="profile-email">{emailUser ? emailUser.email : null}</h3>
+        <img
+          src={ iconProfile }
+          alt="Icone de usuario"
+          id="iconProfile"
+        />
+        <h2 id="textProfile">Profile</h2>
+        <h3
+          id="email"
+          data-testid="profile-email"
+        >
+          {emailUser ? emailUser.email : null}
+
+        </h3>
         <section>
-          <button
-            type="button"
-            data-testid="profile-done-btn"
-            onClick={ () => this.handleClick('done-recipes') }
-          >
-            Done Recipes
+          <div id="div1">
+            <button
+              className="btn"
+              type="button"
+              data-testid="profile-done-btn"
+              onClick={ () => this.handleClick('done-recipes') }
+            >
+              <img
+                src={ iconDone }
+                alt="Icone de usuario"
+                className="img"
+              />
+              Done Recipes
 
-          </button>
-          <button
-            type="button"
-            data-testid="profile-favorite-btn"
-            onClick={ () => this.handleClick('favorite-recipes') }
-          >
-            Favorite Recipes
+            </button>
+          </div>
+          <div id="div2">
+            <button
+              className="btn"
+              type="button"
+              data-testid="profile-favorite-btn"
+              onClick={ () => this.handleClick('favorite-recipes') }
+            >
+              <img
+                src={ iconFav }
+                alt="Icone de usuario"
+                className="img"
+              />
+              Favorite Recipes
 
-          </button>
-          <button
-            type="button"
-            data-testid="profile-logout-btn"
-            onClick={ () => this.clickLogout() }
-          >
-            Logout
+            </button>
+          </div>
+          <div id="div3">
+            <button
+              className="btn"
+              type="button"
+              data-testid="profile-logout-btn"
+              onClick={ () => this.clickLogout() }
+            >
+              <img
+                src={ iconLogout }
+                alt="Icone de usuario"
+                className="img"
+              />
+              Logout
 
-          </button>
+            </button>
+          </div>
         </section>
         <footer>
           <Footer />
